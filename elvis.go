2b60e6c0f86704794f4dfa4ea005fcdda6118451
Rob Warner <rob.warner@availity.com>
2017-07-12 18:41:52 -0400

    Add zero value to documentation

--- elvis.go
+++ elvis.go
@@ -2,7 +2,7 @@ package elvis
 
 import "reflect"
 
-// Elvis returns the first argument if it's not nil, else the second argument
+// Elvis returns the first argument if it's not nil or the zero value, else the second argument
 func Elvis(a, b interface{}) interface{} {
 	// Hat tip to https://stackoverflow.com/users/187676/erik-aigner and https://stackoverflow.com/questions/13901819/quick-way-to-detect-empty-values-via-reflection-in-go
 	return Ternary(a == nil || reflect.DeepEqual(a, reflect.Zero(reflect.TypeOf(a)).Interface()), b, a)
